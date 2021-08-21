5c5a761b78b73822ecf6825fad7835f6bf253ce6
itsmenexus23 <87323493+itsmenexus23@users.noreply.github.com>
2021-08-21 17:45:25 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,41 @@
+#DINOSAUR CHEAT GOOGLE!!!!!!!
+#AUTHOR: ITSMENEXUS23
+#READ readme.MD
+
+from PIL import ImageGrab
+from PIL import ImageOps
+import pyautogui
+import time
+import keyboard
+from numpy import *
+
+replaybtn = (960, 438)
+dinosaur = (724, 456)
+
+
+
+def restartGame():
+    pyautogui.click(replaybtn)
+    print("Happy Jumping Dino!")
+
+
+def image_grab():
+    box = (dinosaur[0]+55, dinosaur[1], dinosaur[0]+145, dinosaur[1]+5)
+    image = ImageGrab.grab(box)
+    grayImage = ImageOps.grayscale(image)
+    a = array(grayImage.getcolors())
+    return a.sum()
+
+def pressSpace():
+    time.sleep(0.1)
+    pyautogui.keyDown('space')
+
+time.sleep(4)
+restartGame()
+
+
+while True:
+    image_grab()
+    if(image_grab() != 697):
+       pressSpace()
+       time.sleep(0.1)
